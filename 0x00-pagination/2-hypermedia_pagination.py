1e5d0754202ad5f4cd801459317c580346cb5cdf
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-23 13:10:37 +0100

    task 0

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 
 import csv
 import math
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Union
 
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -40,7 +40,8 @@ class Server:
         else:
             return []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[Union[int, None, str]]:
         """Return the appropriate page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
