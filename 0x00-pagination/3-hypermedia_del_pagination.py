2d00213c65dadec0e87b8e6e046768ce772ed1ff
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-23 15:52:51 +0100

    task 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -52,12 +52,24 @@ class Server:
         start_index = next((idx for idx,
                             _ in self.__indexed_dataset.items()
                             if idx >= index))
-        if start_index + page_size not in self.__indexed_dataset:
-            next_index = None
+        # if start_index + page_size not in self.__indexed_dataset:
+        #     next_index = None
+        # else:
+        #     next_index = start_index + page_size
+        # end_idx = next_index or len(self.__indexed_dataset)
+        end_index = start_index + page_size - 1
+        num_items = len(self.__indexed_dataset)
+        num_pages = math.ceil(num_items / page_size)
+        current_page = start_index // page_size
+        if current_page < num_pages:
+            next_index = (current_page + 1) * page_size
+            if next_index >= num_items:
+                next_index = None
         else:
-            next_index = start_index + page_size
-        end_idx = next_index or len(self.__indexed_dataset)
-        data = [self.__indexed_dataset[i] for i in range(start_index, end_idx)]
+            next_index = None
+        data = [self.__indexed_dataset[i]
+                for i in range(start_index,
+                               min(end_index + 1, num_items))]
         page_size = len(data)
         return {
             'index': start_index,
