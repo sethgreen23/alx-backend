c86f50069c51d4ea9c097fae8c21722d45cac6ab
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-23 12:38:49 +0100

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,30 +39,28 @@ class Server:
             return self.__dataset[start:end]
         else:
             return []
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         self.dataset()
         total_pages = ((len(self.__dataset) - 1 + page_size) // page_size)
-        start, end = index_range(page, page_size)
         metadata = {
-			'page_size': page_size,
-			'page': page,
-			'data': self.get_page(page, page_size),
-		}
-        
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+        }
+
         if page >= total_pages:
             metadata['next_page'] = None
         else:
             metadata['next_page'] = page + 1
-        
+
         if page <= 1:
             metadata['previeous_page'] = None
         else:
-            metadata['previeous_page'] = page -1
-        
+            metadata['previeous_page'] = page - 1
+
         metadata['total_pages'] = total_pages
         return metadata
-        
