bf988d839b085a1ef361c006c042dfe2ec932cb4
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-23 13:25:42 +0100

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -63,9 +63,9 @@ class Server:
             metadata['next_page'] = page + 1
 
         if page <= 1:
-            metadata['previous_page'] = None
+            metadata['prev_page'] = None
         else:
-            metadata['previous_page'] = page - 1
+            metadata['prev_page'] = page - 1
 
         metadata['total_pages'] = total_pages
         return metadata
