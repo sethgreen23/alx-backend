34a89f1f60f358ee7179b550680e2dc4835051ff
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-25 20:03:53 +0100

    task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,24 +8,23 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """Init method"""
         super().__init__()
-        self.__queue = []
+        self.cache_list = []
 
     def put(self, key, item):
         """Add item to the Fifo cache"""
         if key is None or item is None:
             return
-        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
-           key not in self.cache_data):
-            sorted_dict = {k: v for k, v in sorted(
-                self.cache_data, lambda item: item[1])}
-            old = sorted_dict.pop(next(iter(sorted_dict)))
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            old = self.cache_list.pop(0)
             del self.cache_data[old]
             print(f'DISCARD: {old}')
-        self.__queue.append(key)
         self.cache_data[key] = item
+        self.cache_list.append(key)
 
     def get(self, key):
         """Get item from the cache"""
         if key is None or key not in self.cache_data:
             return None
+        self.cache_list.remove(key)
+        self.cache_list.append(key)
         return self.cache_data.get(key)
