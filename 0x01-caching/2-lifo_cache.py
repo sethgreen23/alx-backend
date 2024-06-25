9437b2a218f046e9c6831448e4e74623e58d0111
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-25 17:49:16 +0100

    task 1

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fifo Cache module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO caching system"""
+    def __init__(self):
+        """Init method"""
+        super().__init__()
+        self.__queue = []
+
+    def put(self, key, item):
+        """Add item to the Fifo cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            old = self.__queue.pop()
+            del self.cache_data[old]
+            print(f'DISCARD: {old}')
+        self.__queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
