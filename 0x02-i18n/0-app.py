e3c4e806a757514b2a71daa9d548a0c17958d61e
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-02 03:32:10 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,10 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/')
 def home_route():
     """Home route"""
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host= "0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
