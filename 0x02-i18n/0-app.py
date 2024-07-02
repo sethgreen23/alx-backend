6be52f9121fba996049d2f9f1aada50af9b6d188
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-02 05:39:52 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def home_route() -> str:
+def home() -> str:
     """Home route"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    app.run(debug=True)
