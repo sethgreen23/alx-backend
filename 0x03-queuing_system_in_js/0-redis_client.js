7e466182bf84b251662d53b3777b12b8328866ac
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-30 00:51:11 +0100

    task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+
+async function connectRedis() {
+	const client = await createClient()
+.on('connect', () => console.log('Redis client connected to the server'))
+.on('error', (err) => console.log('Redis client not connected to the server: ' + err));
+}
+
+connectRedis();
