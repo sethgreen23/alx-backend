8c910bbee1ed9c1a5e74d3ae396aad338a94cf43
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-31 01:26:49 +0100

    task 7

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+const kue = require('kue');
+
+const jobObject = {
+	phoneNumber: '4153518780',
+	message: 'This is the code 1234 to verify your account'
+};
+
+const queue = kue.createQueue();
+
+let job = queue.create('push_notification_code', jobObject).save((err) => {
+	if (!err) {
+		console.log(`Notification job created: ${job.id}`);
+	}
+})
+
+job.on('complete', (result) => {
+	console.log('Notification job completed');
+})
+
+job.on('failed', (err) => {
+	console.log(`Notification job failed`);
+});
